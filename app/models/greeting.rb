0b8d63f3b10f062365c8a9f826323f3b64b106f4
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-09-13 15:24:30 +0200

    Create Greeting model

--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,2 +1,10 @@
 class Greeting < ApplicationRecord
+  validates :message, presence: true
+
+  def self.message
+    messages = Greeting.all.sample(1)
+    return messages.first.slice(:message) unless messages.empty?
+
+    { message: 'There are no greeting messages' }
+  end
 end
